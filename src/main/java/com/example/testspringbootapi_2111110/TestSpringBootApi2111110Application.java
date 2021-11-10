215f8714e51bc0a0242f9f8e7da057a126f221c7
hideru <jota.16.stigma.world@gmail.com>
2021-11-10 14:17:47 +0900

    Hello World Failed

---
+++ src/main/java/com/example/testspringbootapi_2111110/TestSpringBootApi2111110Application.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootapi_2111110;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootApi2111110Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBootApi2111110Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringbootapi_2111110/TestSpringBootApi2111110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootapi_2111110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootApi2111110ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
