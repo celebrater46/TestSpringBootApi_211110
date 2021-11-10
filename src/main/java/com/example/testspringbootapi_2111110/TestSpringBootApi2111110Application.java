58b4564eb138379a1eeb0d8889b3fd86376f2bbf
hideru <jota.16.stigma.world@gmail.com>
2021-11-10 14:24:45 +0900

    Added Refer:

--- src/main/java/com/example/testspringbootapi_2111110/TestSpringBootApi2111110Application.java
+++ src/main/java/com/example/testspringbootapi_2111110/TestSpringBootApi2111110Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestSpringBootApi2111110Application {
 
+    // Refer: https://spring.pleiades.io/guides/gs/rest-service/
     public static void main(String[] args) {
         SpringApplication.run(TestSpringBootApi2111110Application.class, args);
 //        SpringApplication.run(GreetingController.class, args); // Process finished
